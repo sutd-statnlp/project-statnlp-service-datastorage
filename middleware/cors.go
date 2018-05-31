b516a8bfaef71cc07b2f0e5c413d1d3e82f96ff5
Ai Nguyen <ainguyenkaka@gmail.com>
2018-05-31 17:53:29 +0800

    update cors content-type

--- middleware/cors.go
+++ middleware/cors.go
@@ -12,8 +12,8 @@ import (
 func Cors() gin.HandlerFunc {
 	return cors.New(cors.Config{
 		AllowAllOrigins:  true,
-		AllowMethods:     []string{"GET", "POST", "PUT", "PATCH", "DELETE"},
-		AllowHeaders:     []string{"Origin"},
+		AllowMethods:     []string{"GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"},
+		AllowHeaders:     []string{"Origin", "Content-Type"},
 		ExposeHeaders:    []string{"Content-Length"},
 		AllowCredentials: true,
 		MaxAge:           12 * time.Hour,
